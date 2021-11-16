db3e86e26aac4ca0e21deafe0d3d1b11b0ec2113
jhonatan O <jhonatan.ortega.joaqui@correounivalle.edu.co>
2021-11-16 10:02:09 -0500

    metaData for test and fixtures

--- tests/firstMetaScript.js
+++ tests/firstMetaScript.js
@@ -3,10 +3,20 @@ import { Selector } from "testcafe";
 const developerName = Selector("#developer-name");
 const osOption = Selector("#windows");
 const submitButton = Selector("#submit-button");
-fixture('First Fixture')
+fixture.meta('version', 1)('First Fixture')
     .page("https://devexpress.github.io/testcafe/example/");
 
-test.page("https://devexpress.github.io/testcafe/example/")
+test.meta('env', 'prod')
+.page("https://devexpress.github.io/testcafe/example/")
+    ('First Test', async t => {
+        await t
+            .typeText(developerName, 'John Smith')
+            .click(osOption)
+            .click(submitButton)
+            .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+    });
+test
+.page("https://devexpress.github.io/testcafe/example/")
     ('First Test', async t => {
         await t
             .typeText(developerName, 'John Smith')
